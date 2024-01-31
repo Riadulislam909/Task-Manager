2b03b96717b996039dd775293252b84d6ca8ba50
Riadulislam909 <riadulislam814@gmail.com>
2024-01-31 14:40:34 +0600

    split our html templet in proper components and showing task list from array

---
+++ src/components/task/TaskBoard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+
+const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learning React",
+    description: "React is most popular library fro frontend development",
+    tags: ["React", "JavaScript", "web"],
+    priority: "High",
+    isFavourite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  return (
+    <>
+      <section className="mb-20" id="tasks">
+        <div className="container">
+          <SearchTask />
+          <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+            <TaskActions />
+            <TaskList tasks={tasks} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TaskBoard;
